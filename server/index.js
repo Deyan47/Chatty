cb28d1bb54f750fcfe694fc973251fa736f96706
Deyan V. Yonkov <deqnyonkov47@gmail.com>
2021-10-12 23:21:00 +0200

    Finishing

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const PORT = process.env.PORT || 5000;
 
 require("dotenv").config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded());
 
