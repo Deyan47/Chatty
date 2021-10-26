a9e4343dfa35440d86604c58723594eaa026e617
Deyan V. Yonkov <deqnyonkov47@gmail.com>
2021-10-26 23:08:19 +0200

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -8,14 +8,18 @@ const PORT = process.env.PORT || 5000;
 
 require("dotenv").config();
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
